03cfda9f2df2c1db6708bb5f687b8fd64482961e
MundatiC <calebngethe230@gmail.com>
2023-06-14 16:31:21 +0300

    fixes routes import path in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 
-const router = require('./routes/bookRoutes')
+const router = require('./src/routes/bookRoutes')
 
 const app = express();
 
