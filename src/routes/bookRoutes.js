39fd9b9b1d7aea56eef17bd111e4eb2bd238e51a
ClarenceG01 <gatama001@gmail.com>
2023-06-14 16:51:26 +0300

    display books by id

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const router = express.Router();
-const { displayAllBooks } = require("../controllers/bookControllers");
+const {
+  displayAllBooks,
+  displayBookById,
+} = require("../controllers/bookControllers");
 
 router.get("/books", displayAllBooks);
-
+router.get("/books/:id", displayBookById);
 module.exports = router;
