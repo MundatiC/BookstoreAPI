6fa302c56a0b910a8ba2b49b05fca853cfc1639c
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-06-18 21:34:46 +0300

    adds authorization requirements to the member controllers

--- src/controllers/memberControllers.js
+++ src/controllers/memberControllers.js
@@ -1,112 +1,243 @@
 const mssql = require("mssql");
+
 const config = require("../config/config");
+
 const bcrypt = require("bcrypt");
 
+
+
+const getAUser = require('../utils/getAMember')
+
+const { tokenGenerator } = require('../utils/token')
+
+
+
 async function getMemberById(req, res) {
+
   const { id } = req.params;
+
+
 
   let sql = await mssql.connect(config);
 
+
+
   if (sql.connected) {
+
     const request = sql.request();
+
+
 
     request.input("MemberID", id);
 
+
+
     let result = await request.execute("GetMemberByIdProcedure");
 
+
+
     if (result.recordset.length > 0) {
+
       res.json({
+
         success: true,
+
+
 
         message: "Retrieved member successfully",
 
+
+
         data: result.recordset[0],
+
       });
+
     } else {
+
       res.status(404).json({
+
         success: false,
 
+
+
         message: "Member not found",
+
       });
+
     }
+
   }
+
 }
 
+
+
 async function getMembersWithLoans(req, res) {
+
   let sql = await mssql.connect(config);
 
+
+
   if (sql.connected) {
+
     const request = sql.request();
+
+
 
     let result = await request.execute("GetMembersWithLoansProcedure");
 
+
+
     res.json({
+
       success: true,
+
+
 
       message: "Retrieved members with loans successfully",
 
+
+
       data: result.recordset,
+
     });
+
   }
+
 }
+
 async function registerUser(req, res) {
-  const { Name, Address, ContactNumber, Password } = req.body;
+
+  let user = req.body;
+
+  // let salt = await bycrypt.genSalt(8);
+
+  // let hashed_pwd = await bycrypt.hash(user.Password, salt)
+
+  let hashed_pwd = await bcrypt.hash(user.Password, 8);
+
+
+
+
+
   let sql = await mssql.connect(config);
-  let hashed_password = await bcrypt.hash(Password, 8);
-  console.log(Password);
-  console.log(hashed_password);
+
+
+
   if (sql.connected) {
-    let result = await sql
-      .request()
-      .input("Name", Name)
-      .input("Address", Address)
-      .input("ContactNumber", ContactNumber)
-      .input("Password", hashed_password)
-      .execute("registerUser");
-    let all_users = await sql
-      .request()
-      .input("Name", Name)
-      .execute("getRegisteredUser");
-    res.status(200).json({
-      success: true,
-      message: "Registered Successfully",
-      result: all_users.recordset,
-    });
+
+    let results = await sql.request()
+
+      .input("Name", user.Name)
+
+      .input("Address", user.Address)
+
+      .input("ContactNumber", user.ContactNumber)
+
+      .input("Password", hashed_pwd)
+
+      .execute("InsertMemberProcedure")
+
+
+
+    console.log(results)
+
+    res.send(results)
+
+
+
   }
+
 }
+
 async function loginUser(req, res) {
+
   let { MemberID, Password } = req.body;
+
   try {
-    let sql = await mssql.connect(config);
-    if (sql.connected) {
-      let result = await sql
-        .request()
-        .input("MemberID", MemberID)
-        .execute("getMemberByID");
-      let user = result.recordset[0];
-      if (user) {
-        let passwordMatch = await bcrypt.compare(Password, user.Password);
-        console.log(user);
-        passwordMatch
-          ? res.json({ success: true, message: "logged in successfully" })
-          : res.json({ success: false, message: "Wrong Credetnials" });
-      } else {
+
+    let user = await getAUser(MemberID);
+
+
+
+    if (user) {
+
+      let passwords_match = await bcrypt.compare(Password, user.Password);
+
+      if (passwords_match) {
+
+
+
+        let token = await tokenGenerator({
+
+          MemberID: user.MemberID,
+
+
+
+        })
+
+        console.log(token)
+
+
+
+
+
         res.json({
+
+          success: true,
+
+          message: "log in successful",
+
+          token
+
+        })
+
+      } else {
+
+        res.status(401).json({
+
           success: false,
-          message: "User not found",
-        });
+
+          message: "wrong credentials"
+
+        })
+
       }
+
+
+
     } else {
-      res.status(500).json({
+
+      res.status(404).json({
+
         success: false,
-        message: "Internal Server Error",
-      });
+
+        message: "No user found"
+
+      })
+
     }
-  } catch (error) {}
+
+
+
+  }
+
+  catch (error) {
+
+
+
+  }
+
 }
+
 module.exports = {
+
   getMemberById,
+
   getMembersWithLoans,
+
   registerUser,
+
   loginUser,
+
 };
