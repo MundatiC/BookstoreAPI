fd89947a19c0094697d982d4889730c2f8fc694a
MundatiC <calebngethe230@gmail.com>
2023-06-26 10:05:42 +0300

    adds some styles to home-page

--- client/interface/src/components/Home.js
+++ client/interface/src/components/Home.js
@@ -4,6 +4,24 @@ import "../home.css"
 import mainbanner1 from "../images/main-banner1.jpg";
 import TopBar from "./TopBar";
 import Navbar from "./Navbar";
+import kigogo from "../images/kigogo.png";
+
+const bookCards = [];
+for (let i = 0; i < 4; i++) {
+  bookCards.push(
+    <div className="book-card" key={i}>
+      <img src={kigogo} alt="" />
+      <h6>
+        Author:
+        <span>Pauline Kea</span>
+      </h6>
+      <h6>
+        Publication Year:
+        <span>2018</span>
+      </h6>
+    </div>
+  );
+}
 
 function Home() {
   return (
@@ -17,14 +35,12 @@ function Home() {
   <div className="row">
     <div className="col-md-12">
 
-      <button className="prev slick-arrow">
-        <i className="icon icon-arrow-left"></i>
-      </button>
+     
 
       <div className="main-slider pattern-overlay">
         <div className="slider-item">
           <div className="banner-content">
-            <h2 className="banner-title">Life of the Wild</h2>
+            <h2 className="banner-title">Welcome to Nuria Library</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.</p>
             <div className="btn-wrap">
              
@@ -34,18 +50,25 @@ function Home() {
         </div>
       </div>
         
-      <button className="next slick-arrow">
-        <i className="icon icon-arrow-right"></i>
-      </button>
+      
       
     </div>
   </div>
 </div>
 
+<div className="available-books">
+      {/* create jsx for a grid tempalte*/}
+      <h3>Featured Books</h3>
+      <div className="book-grid">{bookCards}</div>
+    </div>
+
 
 </section>
 
+
     </div>
+    
+    
      
     </>
   );
---
+++ client/interface/src/components/SingleBook.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./singleBook.css";
+import bookImage from "./product-item3.jpg";
+
+const SingleBook = () => {
+  return (
+    <section className="bg-sand padding-large">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <a href="#" className="product-image">
+              <img src={bookImage} alt="Book Image" />
+            </a>
+          </div>
+
+          <div className="col-md-6 pl-5">
+            <div className="product-detail">
+              <h1>Simple Way of Peace</h1>
+              <p>Fiction</p>
+
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.
+              </p>
+              <p>
+                Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </p>
+
+              <button type="submit" name="borrow" value="27545" className="button">
+                Borrow
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SingleBook;
