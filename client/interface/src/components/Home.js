1d032a7dd996169f12c20201a62a6a2cb3dc5924
MundatiC <calebngethe230@gmail.com>
2023-06-26 10:10:19 +0300

    adds some styles to home-page

--- client/interface/src/components/Home.js
+++ client/interface/src/components/Home.js
@@ -41,7 +41,7 @@ function Home() {
         <div className="slider-item">
           <div className="banner-content">
             <h2 className="banner-title">Welcome to Nuria Library</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu feugiat amet, libero ipsum enim pharetra hac. Urna commodo, lacus ut magna velit eleifend. Amet, quis urna, a eu.</p>
+            <p> Discover a world of knowledge and imagination at your fingertips. Explore our vast collection of books, embark on literary adventures, and expand your horizons. Whether you're seeking entertainment, education, or inspiration, our library is here to accompany you on your journey. Start your reading adventure today and let the magic of books transport you to new realms of discovery. Happy reading!</p>
             <div className="btn-wrap">
              
             </div>
