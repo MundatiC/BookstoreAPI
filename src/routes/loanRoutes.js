d61fd28f12cd1799522d77eacdfcbfad5b27320d
MundatiC <calebngethe230@gmail.com>
2023-06-27 11:59:02 +0300

    adds endpoints for overdue, returned, and borrowed books for a user

--- src/routes/loanRoutes.js
+++ src/routes/loanRoutes.js
@@ -4,12 +4,19 @@ const loanRouter = express.Router();
 const tokenValidateMiddleware = require('../middlewares/tokenValidateMiddleware');
 const {
     borrowBook,
-    returnBook
+    returnBook,
+    GetBorrowedBooksByUser,
+    GetReturnedBooksByUser,
+    OverdueBooksByUser
 } = require("../controllers/loanControllers");
 
 loanRouter.post("/borrow", tokenValidateMiddleware, borrowBook);
 
 loanRouter.post("/return",tokenValidateMiddleware, returnBook)
+
+loanRouter.get("/overdueloans",tokenValidateMiddleware, OverdueBooksByUser)
+loanRouter.get("/borrowedBooksByID",tokenValidateMiddleware, GetBorrowedBooksByUser)
+loanRouter.get("/returnedbooks",tokenValidateMiddleware, GetReturnedBooksByUser)
 
 
 
